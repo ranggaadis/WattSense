4199201e4fe007718fa84477c7f36dd87957dc10
ranggaadis <adistakirista@gmail.com>
2025-12-10 01:15:13 +0700

    Preparing for Deployment

--- middleware.js
+++ middleware.js
@@ -1,13 +1,6 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import { createMiddleware } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/account(.*)",
-  "/transaction(.*)",
-]);
-
 
 // Create base Clerk middleware
 const clerk = clerkMiddleware(async (auth, req) => {
